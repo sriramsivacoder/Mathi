222b9cd5a4b15a8d018a631fb9c270e221404b12
Dhruva-Webcraft <dhruvawebcraftkce@gmail.com>
2026-03-29 12:36:45 +0530

    Image replaced

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,7 +9,7 @@ const Gallery = () => {
     "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070&auto=format&fit=crop",
     "https://images.unsplash.com/photo-1540497077202-7c8a3999166f?q=80&w=2070&auto=format&fit=crop",
     "https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?q=80&w=2070&auto=format&fit=crop",
-    "https://images.unsplash.com/photo-1593079831268-3381b0c1356a?q=80&w=2069&auto=format&fit=crop",
+    "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?q=80&w=2070&auto=format&fit=crop",
     "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?q=80&w=2070&auto=format&fit=crop",
     "https://images.unsplash.com/photo-1518611012118-696072aa579a?q=80&w=2070&auto=format&fit=crop"
   ];
